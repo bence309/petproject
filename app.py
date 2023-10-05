c18a9a4d71824e1a739877bbfb7827476351f126
Bence Bodi <bence309@hotmail.com>
2023-10-05 20:31:15 +0200

    Using postgreSQL instead of SQlite. App is working fine. Added .env and .gitignore files.

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__, template_folder="templates")
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 
-class Todo(db.Model) : 
-    task_id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100))
-    done=db.Column(db.Boolean)
+class Todo(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    done = db.Column(db.Boolean)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
