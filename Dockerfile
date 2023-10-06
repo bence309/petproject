55b1b2ce69b514dec041ac741054fe3c9df048ef
Bence Bodi <bence309@hotmail.com>
2023-10-06 14:06:38 +0200

    optimized for docker It's working from docker image

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+
 EXPOSE 80
 
 ENV NAME World
--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def delete(todo_id):
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
