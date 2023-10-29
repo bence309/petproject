89f6efb0be15cbe99fa1cf15f688643fb88f084d
Bence Bodi <bence309@hotmail.com>
2023-10-29 16:38:41 +0100

    app.py file refactored, ci/cd test

--- app.py
+++ app.py
@@ -20,33 +20,30 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    todo_list=Todo.query.all()
+    todo_list = Todo.query.all()
     return render_template('index.html', todo_list=todo_list)
-
 
 @app.route('/add', methods=['POST'])
 def add():
-    name=request.form.get("name")
-    new_task=Todo(name=name, done=False)
+    name = request.form.get("name")
+    new_task = Todo(name=name, done=False)
     db.session.add(new_task)
     db.session.commit()
     return redirect(url_for("index"))
 
-
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    todo=Todo.query.get(todo_id)
-    todo.done=not todo.done
+    todo = Todo.query.get(todo_id)
+    todo.done = not todo.done
     db.session.commit()
     return redirect(url_for("index"))
 
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    todo=Todo.query.get(todo_id)
+    todo = Todo.query.get(todo_id)
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
