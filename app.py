c35cdf353042c749c894e7620722a3241653017d
Bence Bodi <bence309@hotmail.com>
2023-10-29 16:42:43 +0100

    another app.py refactor for PEP8

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def index():
     todo_list = Todo.query.all()
     return render_template('index.html', todo_list=todo_list)
 
+
 @app.route('/add', methods=['POST'])
 def add():
     name = request.form.get("name")
@@ -31,6 +32,7 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
     todo = Todo.query.get(todo_id)
@@ -38,12 +40,14 @@ def update(todo_id):
     db.session.commit()
     return redirect(url_for("index"))
 
+
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
     todo = Todo.query.get(todo_id)
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
